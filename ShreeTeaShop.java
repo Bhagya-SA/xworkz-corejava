7a127b8867227c1d4787b6f3aee636dcf243c353
Bhaya-SA <bhagyaanjaneya@gmail.com>
2026-02-09 06:09:41 +0530

    Array examples 2

---
+++ ShreeTeaShop.java
@@ -0,0 +1,11 @@
+class ShreeTeaShop {
+	public static void main(String[] args) {
+		String differentTypesOfTeas[] = {"White Tea" , "Green Tea" , "Yellow Tea" , "Oolong Tea" , "Black Tea" , "Dark Tea" , "Herbal Tea" , "Yerba Tea" , "Rooibos" , "Matcha" , "Chai"};
+		
+	    for( String tea : differentTypesOfTeas) {
+			System.out.println(tea);
+			
+		}
+	}
+
+}
